db58a9b0262b8a35396150985bcdb28a4094ab97
olegrozhman <oskol.reklama@mail.ru>
2018-06-27 07:51:24 +0300

    additional files and changes for Heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'btv%bhobux6glp3zq)z43pwp_*r1cr6x90s$lh+x)ra$-c*f42'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'rozhman.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'heroku.app.com']
 
 
 # Application definition
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycorp2',
+        'NAME': 'djangorozhok',
+        'USER': 'rozhok',
+        'PASSWORD': '1320ae',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -120,3 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/'
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
